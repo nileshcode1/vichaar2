99aea16b64be5aa3241dade4a66ee44a29e6e74b
nileshcode1 <itsoffcialcode@gmail.com>
2024-02-06 00:48:23 +0530

    removing domain

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -78,7 +78,7 @@ const WritePage = () => {
       .replace(/^-+|-+$/g, "");
 
   const handleSubmit = async () => {
-    const res = await fetch("https://vichaar2.vercel.app/api/posts", {
+    const res = await fetch("/api/posts", {
       method: "POST",
       body: JSON.stringify({
         title,
